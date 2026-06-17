668c0e83204a6dc78e9b013bff8148df16770e32
david stern <dzs101010@gmail.com>
2026-06-17 15:36:53 +0300

    Almost before the end of the class

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,55 +2,67 @@ import mysql.connector
 
 class DB_connection:
 
-    def __init__(self, conn):
-        self.conn = conn
+    def __init__(self):
+        self.host = "localhost"
+        self.user = "root"
+        self.password = "1234"
+        self.database = "intelligence-mysql"
 
-    def get_connection(self, conn):
-        conn = mysql.connector.connect(
-        host = "localhost",
-        password = "1234",
-        database = "intelligence-mysql"
-        )
+    def get_connection(self):
+        config = {"host": self.host,
+                  "user": self.user,
+                  "password": self.password,
+                  "database": self.database}
+        conn = mysql.connector.connect(**config)
         return conn
 
 
+
     def create_database(self):
-        conn = self.conn
+        config = {"host": self.host,
+                  "user": self.user,
+                  "password": self.password}
+        conn = mysql.connector.connect(**config)
         cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE Intelligence_db IF NOT EXISTS")
+        cursor.execute("CREATE DATABASE IF NOT EXISTS Intelligence_db")
 
 
     def create_tables(self):
-        conn = self.conn
+        conn = self.get_connection()
         cursor = conn.cursor()
         cursor.execute(
             """
-            CREATE TABLE agents IF NOT EXISTS(
+            CREATE TABLE IF NOT EXISTS agents(
             id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(100) NOT NULL,
             specialty VARCHAR(100) NOT NULL,
             is_active BOOLEAN DEFAULT TRUE,
             completed_missions INT DEFAULT 0,
             failed_missions INT DEFAULT 0,
-            agent_rank ENUM('Junior', 'Senior', 'Commander'))
+            agent_rank ENUM('Junior', 'Senior', 'Commander') NOT NULL)
             """
         )
-        cursor.commit()
+        conn.commit()
         cursor.execute(
             """
-            CREATE TABLE missions IF NOT EXISTS(
+            CREATE TABLE IF NOT EXISTS missions(
             id INT AUTO_INCREMENT PRIMARY KEY,
             title VARCHAR(100) NOT NULL,
             description TEXT(100) NOT NULL,
             location VARCHAR(100) NOT NULL,
-            difficulty INT(10) NOT NULL,
-            importance INT(10) NOT NULL,
-            status ENUM('NEW', 'ASSIGNED', 'PROGRESS_IN', 'COMPLETED', 'FAILED', 'CANCELLED')
-            level_risk INT difficulty*2 + importance,
+            difficulty INT CHECK(difficulty BETWEEN 1 AND 10) NOT NULL,
+            importance INT CHECK(difficulty BETWEEN 1 AND 10) NOT NULL,
+            status ENUM('NEW', 'ASSIGNED', 'PROGRESS_IN', 'COMPLETED', 'FAILED', 'CANCELLED'),
+            level_risk VARCHAR(100),
             assigned_agent_id INT DEFAULT NULL
             )""")
-        cursor.commit()
+        conn.commit()
         cursor.close()
         conn.close()
 
+db = DB_connection()
+db.get_connection()
 
+if __name__ == "__main__":
+    db.get_connection()
+    db.create_database()
